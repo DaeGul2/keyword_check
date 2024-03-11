2407bca96f7df902d665ba856c51de9f12b34bce
TRASH-TRACO <128914652+TRASH-TRACO@users.noreply.github.com>
2024-03-11 13:08:59 +0900

    fix: padding 추가

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ const DELEMETER = "%구분%"
 const CHECK_STRING_REGEX_STR = "(" + CHECK_STRING.join('|') + ")" ;
 const CHECK_STRING_REGEX = new RegExp(CHECK_STRING_REGEX_STR, 'g');
 
+const PADDING = '10px';
+
 export default function Page() {
     const [text, setText] = useState('')
 
@@ -40,7 +42,8 @@ export default function Page() {
                     height: '300px',
                     resize: 'none',
                     border: '1px solid black',
-                    borderRadius: '5px'
+                    borderRadius: '5px',
+                    padding: PADDING
                 }}
             />
             <div
@@ -49,6 +52,7 @@ export default function Page() {
                     height: '300px',
                     overflow: 'auto',
                     whiteSpace: 'pre-wrap',
+                    padding: `0 ${PADDING}`
                 }}>
                 {
                     splitArr.map((item, index) => {
